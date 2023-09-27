abe7d5c83e234a84c6288353eee40dd452c5b245
Michael Brister <mbrister@new-innov.com>
2023-09-27 12:14:42 -0400

    Neovim configuration based on kickstart

---
+++ lua/custom/plugins/goto-preview.lua
@@ -0,0 +1,8 @@
+return {
+  'rmagatti/goto-preview',
+  config = function()
+    require('goto-preview').setup {}
+  end,
+  vim.keymap.set("n", "gp", "<cmd>lua require('goto-preview').goto_preview_definition()<CR>", { noremap = true }),
+  vim.keymap.set("n", "gP", "<cmd>lua require('goto-preview').close_all_win()<CR>", { noremap = true })
+}
